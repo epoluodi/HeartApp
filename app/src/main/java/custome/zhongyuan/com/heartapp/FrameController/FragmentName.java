e056fde3c3dc202f560aa76ea009fc009625bae8
Apollo Yang <epoluodi@gmail.com>
2017-05-07 21:18:03 +0800

    图标

---
+++ app/src/main/java/custome/zhongyuan/com/heartapp/FrameController/FragmentName.java
@@ -0,0 +1,18 @@
+package custome.zhongyuan.com.heartapp.FrameController;
+
+/**
+ * Created by Administrator on 14-11-30.
+ */
+public interface FragmentName {
+    void SetFragmentName(String name);
+
+    String GetFragmentName();
+
+
+
+
+
+
+
+
+}
